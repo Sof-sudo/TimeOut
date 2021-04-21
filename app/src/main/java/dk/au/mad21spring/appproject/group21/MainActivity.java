91d136850fb752f854fbb99229faf7e10fc7414a
Line Junge <201709965@post.au.dk>
2021-04-21 11:14:03 +0200

    Test commit 2

--- app/src/main/java/dk/au/mad21spring/appproject/group21/MainActivity.java
+++ app/src/main/java/dk/au/mad21spring/appproject/group21/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private int Test;
+    private int Test2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
