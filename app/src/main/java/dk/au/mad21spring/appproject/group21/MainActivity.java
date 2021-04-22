b03ad55e63229a116dd1ee5d747c330cccd50bca
Mads <au579023@uni.au.dk>
2021-04-22 12:43:02 +0200

    Test

--- app/src/main/java/dk/au/mad21spring/appproject/group21/MainActivity.java
+++ app/src/main/java/dk/au/mad21spring/appproject/group21/MainActivity.java
@@ -6,9 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int Test;
-    private int Test2;
-    private int test3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
