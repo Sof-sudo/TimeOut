1c37ccde795ad509b236606e1b57d23cda8f8881
Mads <au579023@uni.au.dk>
2021-04-29 13:18:56 +0200

    Web api + teamlist, virker ikke

--- app/src/main/java/dk/au/mad21spring/appproject/group21/TeamListViewModel.java
+++ app/src/main/java/dk/au/mad21spring/appproject/group21/TeamListViewModel.java
@@ -5,24 +5,24 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import dk.au.mad21spring.appproject.group21.Database.Teams;
 
 public class TeamListViewModel extends ViewModel {
 
-    MutableLiveData<ArrayList<Teams>> teams;
+    private Repository repository;
 
-    public LiveData<ArrayList<Teams>> getTeams() {
-        if(teams == null){
-            teams = new MutableLiveData<>();
+    MutableLiveData<List<Teams>> teams;
 
-            ArrayList<Teams> teamList = new ArrayList<Teams>();
+    public LiveData<List<Teams>> getTeams() {
 
-            teamList.add(new Teams(1, "LAL", "Los Angeles", "West", "Pacific", "Los Angeles Lakers", "Lakers"));
-            teamList.add(new Teams(2, "LAC", "Los Angeles", "West", "Pacific", "Los Angeles Clippers", "Clippers"));
+        return repository.loadAllTeams();
 
-            teams.setValue(teamList);
-        }
-        return teams;
     }
+
+    public void addTeams(){
+        repository.addAllTeams();
+    }
+
 }
