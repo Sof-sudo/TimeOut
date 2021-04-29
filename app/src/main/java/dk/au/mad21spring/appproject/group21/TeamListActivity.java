2cbbc911af9aea8834340c027f0490a23b4215a8
Sofie Bisgaard Hansen <201807880@post.au.dk>
2021-04-29 16:06:14 +0200

    Search

--- app/src/main/java/dk/au/mad21spring/appproject/group21/TeamListActivity.java
+++ app/src/main/java/dk/au/mad21spring/appproject/group21/TeamListActivity.java
@@ -8,6 +8,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.List;
 
@@ -20,6 +23,8 @@ public class TeamListActivity extends AppCompatActivity implements TeamAdapter.I
     // ui widgets
     private RecyclerView rcvList;
     private TeamAdapter adapter;
+    private Button search;
+    private EditText searchField;
 
     // viewmodel
     private TeamListViewModel vm;
@@ -39,6 +44,31 @@ public class TeamListActivity extends AppCompatActivity implements TeamAdapter.I
 
         setupUI();
         vm.addTeams(); // adds all the teams from the API.
+
+        searchField = findViewById(R.id.txtSearch);
+
+        search = findViewById(R.id.btnSearch);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Search();
+            }
+        });
+    }
+
+    private void Search(){
+        String teamSearch = searchField.getText().toString();
+        Team team = vm.searchForTeam(teamSearch);
+
+        if(team!=null){
+            Intent intent = new Intent(this,TeamActivity.class);
+            intent.putExtra(TEAM,team.getName());
+            startActivity(intent);
+        } else {
+            //Fejlbesked: Holdet er ikke at finde i listen
+        }
+
+        searchField.setText("");
     }
 
     private void setupUI() {
