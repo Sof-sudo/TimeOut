cccb1ee9119e50f99d53b2a7b500a7b72611bf44
Martin Bojesen <Au616699@uni.au.dk>
2021-04-29 13:19:29 +0200

    PlayerActivity og PlayerViewModel

--- app/src/main/java/dk/au/mad21spring/appproject/group21/PlayerActivity.java
+++ app/src/main/java/dk/au/mad21spring/appproject/group21/PlayerActivity.java
@@ -1,13 +1,19 @@
 package dk.au.mad21spring.appproject.group21;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -31,8 +37,42 @@ public static final String INDEX = "index";
         Intent data = getIntent();
         int uid = data.getIntExtra(INDEX, 0);
 
-        //playerViewModel = new ViewModelProvider(this).get(PlayerViewModel.class);
+        // Oprettelse af ViewModel
+        playerViewModel = new ViewModelProvider(this).get(PlayerViewModel.class);
+//        playerViewModel.getAll().observe(this, new Observer<List<Players>>() {
+//            @Override
+//            public void onChanged(List<Players> players) {
+//                players.get(uid);
+//                updateUI();
+//            }
+//        });
+
+
+
+        btnBack = findViewById(R.id.BtnBackPlayer);
+        txtPlayers = findViewById(R.id.txtPlayers);
+        txtTeam = findViewById(R.id.txtNamePlayer);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
     }
+
+    private void updateUI() {
+
+//        Glide.with(holder.imgTeamLogo.getContext())
+//                .load("https://www.nba.com/.element/img/1.0/teamsites/logos/teamlogos_500x500/"+TeamList.get(position)
+//                        .getAbbreviation().toLowerCase()+".png").into(holder.imgTeamLogo);
+//        holder.txtTeamName.setText(TeamList.get(position).getFullname());
+
+
+
+    }
+
+
 }
--- app/src/main/java/dk/au/mad21spring/appproject/group21/PlayerViewModel.java
+++ app/src/main/java/dk/au/mad21spring/appproject/group21/PlayerViewModel.java
@@ -1,7 +1,21 @@
 package dk.au.mad21spring.appproject.group21;
 
-public class PlayerViewModel {
+import android.app.Application;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class PlayerViewModel extends ViewModel {
+
+   // private Repository repository;
+
+    public PlayerViewModel(Application app) {
+//repository = Repository.getInsance(app);
+    }
+
+    // public LiveData<List<Players>> getAll() {return repository.LoadData()}
 
 
 }
