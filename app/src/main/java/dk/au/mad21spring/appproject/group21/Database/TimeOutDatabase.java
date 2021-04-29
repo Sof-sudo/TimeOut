f0a3b4ae823b60d162f57d340270c8bf524380b4
Sofie Bisgaard Hansen <201807880@post.au.dk>
2021-04-29 15:05:38 +0200

    teamDao

--- app/src/main/java/dk/au/mad21spring/appproject/group21/Database/TimeOutDatabase.java
+++ app/src/main/java/dk/au/mad21spring/appproject/group21/Database/TimeOutDatabase.java
@@ -8,7 +8,7 @@ import androidx.room.RoomDatabase;
 
 import dk.au.mad21spring.appproject.group21.Database.Dao.GamesDao;
 import dk.au.mad21spring.appproject.group21.Database.Dao.PlayerDao;
-import dk.au.mad21spring.appproject.group21.Database.Dao.TimeOutDao;
+import dk.au.mad21spring.appproject.group21.Database.Dao.TeamDao;
 
 //This code is inspired from lesson 4 and the demo about Persistence
 //This class is a Room class, that implements the singleton pattern and creates the TeamDao class
@@ -16,7 +16,7 @@ import dk.au.mad21spring.appproject.group21.Database.Dao.TimeOutDao;
 @Database(entities = {Team.class, Player.class, Games.class}, version = 2, exportSchema = false)
 public abstract class TimeOutDatabase extends RoomDatabase {
 
-    public abstract TimeOutDao timeOutDao();
+    public abstract TeamDao timeOutDao();
     public abstract PlayerDao playerDao();
     public abstract GamesDao gamesDao();
 
